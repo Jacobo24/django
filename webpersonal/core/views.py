49cc38d19e1f7e79abe9e08567501104718c7cde
Jacobo24 <jaccalgar@gmail.com>
2024-11-18 19:22:35 +0100

    web con imágenes

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-html_base = """
-    <h1>Mi Web Personal</h1>
-    <ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about/">Acerca de</a></li>
-    <li><a href="/contact/">Contacto</a></li>
-    <li><a href="/portfolio/">Portfolio</a></li>
-    </ul>
-"""
+
 def home(request):
     return render(request, "core/home.html")
 
